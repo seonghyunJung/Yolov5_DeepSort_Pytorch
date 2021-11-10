23260e6469d6f4a7e9aa01c56171b18cb9a53b01
Seonghyun Jung <76807684+seonghyunJung@users.noreply.github.com>
2021-11-11 00:29:22 +0900

    Update track4.py

--- track4.py
+++ track4.py
@@ -1,13 +1,13 @@
 # limit the number of cpus used by high performance libraries
+import sys
+sys.path.insert(0, './yolov5')
+
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
-
-import sys
-sys.path.insert(0, './yolov5')
 
 from yolov5.models.experimental import attempt_load
 from yolov5.utils.downloads import attempt_download
